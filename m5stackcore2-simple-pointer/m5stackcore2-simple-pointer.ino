0f7001b6786a9e6e4a373a1830610a510c1d896c
Atsushi Morimoto (74th) <site@74th.tech>
2024-01-29 11:10:44 +0900

    rename method

--- m5stackcore2-simple-pointer/m5stackcore2-simple-pointer.ino
+++ m5stackcore2-simple-pointer/m5stackcore2-simple-pointer.ino
@@ -65,23 +65,6 @@ void set_move_size(int16_t step_dx, int16_t step_dy)
     }
 }
 
-void setup()
-{
-
-#if TESTING > 0
-    Serial.begin(115200);
-    Serial.write("M5Dial I2C Slave\n");
-#else
-    Wire.begin(I2C_SLAVE_ADDRESS, G32, G33, 400000);
-    Wire.onReceive(receiveEvent);
-    Wire.onRequest(sendEvent);
-#endif
-
-    M5.begin();
-    M5.Lcd.fillScreen(BLACK);
-    sprintf(message, "");
-}
-
 long oldPosition = -999;
 
 int prev_x = -1;
@@ -96,14 +79,14 @@ bool first_move = false;
 int t = 0;
 int loop_num = 0;
 
-void show_connection()
+void show_message()
 {
     unsigned long now = millis();
     if (latest_i2c_connection_time + 1000 < now)
     {
         M5.Lcd.setTextDatum(MC_DATUM);
         M5.Lcd.setTextSize(0.5);
-        M5.Lcd.drawString("Connection Error", M5.Lcd.width() / 2, M5.Lcd.height() / 2);
+        M5.Lcd.drawString("no I2C Connection", M5.Lcd.width() / 2, M5.Lcd.height() / 2);
         return;
     }
 
@@ -113,6 +96,80 @@ void show_connection()
         M5.Lcd.setTextSize(0.5);
         M5.Lcd.drawString(message, M5.Lcd.width() / 2, M5.Lcd.height() / 2);
     }
+}
+
+void i2c_receive_event(int numBytes)
+{
+    if (numBytes == 0)
+    {
+        return;
+    }
+
+    uint8_t cmd = Wire.read();
+    int i = 1;
+
+    latest_i2c_command = cmd;
+
+    switch (cmd)
+    {
+    case CMD_POINTER:
+        break;
+    default:
+        sprintf(message, "r: n:%d c:%x", numBytes, cmd);
+        show_message_limit = millis() + 1000;
+        break;
+    }
+
+    for (int i = 1; i < numBytes; i++)
+    {
+        uint8_t b = Wire.read();
+    }
+}
+
+void send_pointer()
+{
+    int n = 0;
+    int i;
+
+    uint8_t *raw_buf = (uint8_t *)&i2c_buf;
+
+    Wire.write(raw_buf[0]);
+    for (i = 1; i < 5; i++)
+    {
+        Wire.write(raw_buf[i]);
+        raw_buf[i] = 0;
+    }
+}
+
+void i2c_send_event()
+{
+    switch (latest_i2c_command)
+    {
+    case CMD_POINTER:
+        send_pointer();
+        break;
+    default:
+        Wire.write(0);
+        break;
+    }
+    latest_i2c_connection_time = millis();
+}
+
+void setup()
+{
+
+#if TESTING > 0
+    Serial.begin(115200);
+    Serial.write("M5Dial I2C Slave\n");
+#else
+    Wire.begin(I2C_SLAVE_ADDRESS, G32, G33, 400000);
+    Wire.onReceive(i2c_receive_event);
+    Wire.onRequest(i2c_send_event);
+#endif
+
+    M5.begin();
+    M5.Lcd.fillScreen(BLACK);
+    sprintf(message, "");
 }
 
 void loop()
@@ -176,62 +233,5 @@ void loop()
         }
     }
 
-    show_connection();
-}
-
-void receiveEvent(int numBytes)
-{
-    if (numBytes == 0)
-    {
-        return;
-    }
-
-    uint8_t cmd = Wire.read();
-    int i = 1;
-
-    latest_i2c_command = cmd;
-
-    switch (cmd)
-    {
-    case CMD_POINTER:
-        break;
-    default:
-        sprintf(message, "r: n:%d c:%x", numBytes, cmd);
-        show_message_limit = millis() + 1000;
-        break;
-    }
-
-    for (int i = 1; i < numBytes; i++)
-    {
-        uint8_t b = Wire.read();
-    }
-}
-
-void send_pointer()
-{
-    int n = 0;
-    int i;
-
-    uint8_t *raw_buf = (uint8_t *)&i2c_buf;
-
-    Wire.write(raw_buf[0]);
-    for (i = 1; i < 5; i++)
-    {
-        Wire.write(raw_buf[i]);
-        raw_buf[i] = 0;
-    }
-}
-
-void sendEvent()
-{
-    switch (latest_i2c_command)
-    {
-    case CMD_POINTER:
-        send_pointer();
-        break;
-    default:
-        Wire.write(0);
-        break;
-    }
-    latest_i2c_connection_time = millis();
+    show_message();
 }
