b17c9df150384540d254bbd0c961952d8128ed41
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-02-12 22:31:14 +0900

    fix esp32s3 build

--- m5dial-simple-pointer/build-merged-firmware.sh
+++ m5dial-simple-pointer/build-merged-firmware.sh
@@ -5,7 +5,7 @@ pio pkg exec -p tool-esptoolpy esptool.py -- --chip esp32s3 merge_bin \
   --flash_mode dio \
   --flash_freq 80m \
   --flash_size 8MB \
-  0x1000 .pio/build/m5dial/bootloader.bin \
+  0x0000 .pio/build/m5dial/bootloader.bin \
   0x8000 .pio/build/m5dial/partitions.bin \
   0xe000 ~/.platformio/packages/framework-arduinoespressif32/tools/partitions/boot_app0.bin \
   0x10000 .pio/build/m5dial/firmware.bin
